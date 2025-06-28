3f84100d10491e9ce59ff1cb4e560a5648311ff8
Angel-Miro <angelcougar96@gmail.com>
2025-06-27 19:27:28 -0600

    add advice controller config

--- src/main/java/com/core/ajmg/controller/ErrorHandlerController.java
+++ src/main/java/com/core/ajmg/controller/ErrorHandlerController.java
@@ -1,6 +1,7 @@
 package com.core.ajmg.controller;
 
 import com.core.ajmg.dto.Error;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,7 +14,7 @@ public class ErrorHandlerController {
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<Error> genericResponse(Exception ex) {
-        return ResponseEntity.badRequest().body(new Error(400, "Error en el parametro", LocalDateTime.now()));
+        return ResponseEntity.badRequest().body(new Error(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), LocalDateTime.now()));
     }
 
 }
